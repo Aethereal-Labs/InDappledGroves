711c5dd845fa6ac4d26a52ccef6e6d19bc383fa7
Vinter Nacht <vinternacht1@gmail.com>
2025-08-28 11:17:38 -0500

    Revert "Updates"
    This reverts commit 17935ee8c7dbdb5eab4e766ca36c13dec9309572.

--- InDappledGroves/BlockEntities/IDGBEWorkstation.cs
+++ InDappledGroves/BlockEntities/IDGBEWorkstation.cs
@@ -324,7 +324,6 @@ namespace InDappledGroves.BlockEntities
 
         public override void GetBlockInfo(IPlayer forPlayer, StringBuilder dsc)
         {
-
             var primary = Block.Variant["primary"];
             var secondary = Block.Variant["secondary"];
             var materials = (Lang.Get("material-" + $"{primary}") + (secondary != null ? " and " + Lang.Get("material-" + $"{secondary}") : ""));
